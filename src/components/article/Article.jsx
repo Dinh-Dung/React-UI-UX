6b491ebeb10a425856455e58fa185556dc5b001d
Dung-Dinh <dungvanbmt.01@gmail.com>
2022-11-23 11:52:42 +0700

    build cta with blog components

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './article.css'
-const Article = () => {
+const Article = ({imgUrl,date,text}) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-contianer_article'>
+        <div className='gpt3__blog-container_article-img'>
+          <img src={imgUrl} alt="blog_image" />
+        </div>
+        <div className='gpt3__blog-container_article-content'>
+          <div>
+            <p>{text}</p>
+            <h3>{text}</h3>
+          </div>
+          <p>Read Full Article</p>
+        </div>
+    </div>
   )
 }
 
